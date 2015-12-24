ef5bad72a9b246c5a66b0f267ba9b65c0f535013
BenFradet <benjamin.fradet@gmail.com>
2015-12-24 14:29:20 +0100

    removed unused import

--- titanic/src/main/scala/com/github/benfradet/Titanic.scala
+++ titanic/src/main/scala/com/github/benfradet/Titanic.scala
@@ -4,7 +4,7 @@ import org.apache.log4j.{Logger, Level}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.RandomForestClassifier
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
-import org.apache.spark.ml.feature.{StringIndexerModel, IndexToString, VectorAssembler, StringIndexer}
+import org.apache.spark.ml.feature.{IndexToString, VectorAssembler, StringIndexer}
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
