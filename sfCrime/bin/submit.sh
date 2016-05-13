7da8840e3c1aeeb53af8f67721ee01d96df0660d
BenFradet <benjamin.fradet@gmail.com>
2016-05-13 19:38:12 +0200

    submit script for the sf crime dataset

---
+++ sfCrime/bin/submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+OUTPUT="classified.csv"
+TMP_FILE="${OUTPUT}2"
+
+sbt "project sfCrime" clean assembly
+
+cd ${DIR}
+rm -rf ${OUTPUT}
+rm -rf ${TMP_FILE}
+
+spark-submit \
+  --class io.github.benfradet.SFCrime \
+  --master local[2] \
+  target/scala-2.11/sfCrime-assembly-1.0.jar \
+  src/main/resources/train.csv src/main/resources/test.csv ${OUTPUT}
+
+mv ${OUTPUT}/part-00000 ${TMP_FILE}
+rm -rf ${OUTPUT}
+mv ${TMP_FILE} ${OUTPUT}
