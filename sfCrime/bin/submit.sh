92085930b3c42a4f6771adf6d6a6afd3bfc79ee7
BenFradet <benjamin.fradet@gmail.com>
2016-05-17 19:02:37 +0200

    adapted the submit script to work with zipped datasets

--- sfCrime/bin/submit.sh
+++ sfCrime/bin/submit.sh
@@ -10,12 +10,22 @@ cd ${DIR}
 rm -rf ${OUTPUT}
 rm -rf ${TMP_FILE}
 
+RESOURCES_PATH="src/main/resources/"
+TRAIN_FILE="train.csv"
+TRAIN_PATH="${RESOURCES_PATH}${TRAIN_FILE}"
+TEST_FILE="test.csv"
+TEST_PATH="${RESOURCES_PATH}${TEST_FILE}"
+
+unzip ${TRAIN_PATH} -d ${RESOURCES_PATH}
+unzip ${TEST_PATH} -d ${RESOURCES_PATH}
+
 spark-submit \
   --class io.github.benfradet.SFCrime \
   --master local[2] \
   target/scala-2.11/sfCrime-assembly-1.0.jar \
-  src/main/resources/train.csv src/main/resources/test.csv ${OUTPUT}
+  ${TRAIN_PATH} ${TEST_PATH} ${OUTPUT}
 
 mv ${OUTPUT}/part-00000 ${TMP_FILE}
 rm -rf ${OUTPUT}
 mv ${TMP_FILE} ${OUTPUT}
+rm -rf ${TRAIN_PATH} ${TEST_PATH}
