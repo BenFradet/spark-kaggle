11b0d0a38fc035e2935c2e15cd4323dd0975e107
BenFradet <benjamin.fradet@gmail.com>
2016-05-29 18:09:12 +0200

    script building the sunrise and sunset dataset

---
+++ sfCrime/src/main/scala/io/github/benfradet/BuildSetRiseDataset.scala
@@ -0,0 +1,85 @@
+package io.github.benfradet
+
+import java.io.FileWriter
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+import cats.data.Xor
+import com.twitter.util.{TimerTask, Duration, JavaTimer}
+import io.circe.Decoder
+import io.circe.generic.auto._
+import io.circe.parser.decode
+import io.circe.syntax._
+
+import scala.collection.mutable
+import scala.io.Source
+
+sealed trait SunSetRiseTrait {
+  def sunrise: Option[String]
+  def sunset: Option[String]
+}
+case class SunSetRise(sunrise: Option[String], sunset: Option[String]) extends SunSetRiseTrait
+case class SunSetRiseDate(date: String, sunrise: Option[String], sunset: Option[String])
+  extends SunSetRiseTrait
+
+object SunSetRise {
+  implicit val decodeSunSetRise: Decoder[SunSetRise] = Decoder.instance { c =>
+    for {
+      res <- c.downField("results").as[Map[String, String]]
+    } yield SunSetRise(res.get("sunrise"), res.get("sunset"))
+  }
+}
+
+/**
+ * Build a dataset containing time of sunset and sunrise for the different dates contained in the
+ * SF crime dataset.
+ */
+object BuildSetRiseDataset {
+
+  def main(args: Array[String]): Unit = {
+    val sfLat = 37.7749d
+    val sfLng = 122.4194d
+
+    val minDate = LocalDate.of(2003, 1, 1)
+    val maxDate = LocalDate.of(2015, 5, 13)
+    val dateFormatter = DateTimeFormatter.ISO_LOCAL_DATE
+
+    val urlTemplate = "http://api.sunrise-sunset.org/json?lat=%f&lng=%f&date=%s"
+    val urlsQeueue = mutable.Queue(
+      daysBetween(minDate, maxDate)
+        .map { d =>
+          val formattedDate = d.format(dateFormatter)
+          (formattedDate, urlTemplate.format(sfLat, sfLng, formattedDate))
+        }: _*
+    )
+    require(urlsQeueue.nonEmpty, "there should be URLs to query")
+
+    val fileWriter = new FileWriter("sfCrime/src/main/resources/sunsetrise.json")
+    fileWriter.write("[\n")
+
+    val timer = new JavaTimer(isDaemon = false)
+    lazy val task: TimerTask = timer.schedule(Duration.fromSeconds(5)) {
+      val (date, url) = urlsQeueue.dequeue()
+      val sunSetRise = decode[SunSetRise](Source.fromURL(url).mkString)
+        .map(s => SunSetRiseDate(date, s.sunrise, s.sunset))
+      sunSetRise match {
+        case Xor.Right(s) =>
+          val json = s.asJson.spaces2
+          if (urlsQeueue.isEmpty) {
+            task.cancel()
+            timer.stop()
+            fileWriter.write(s"$json\n")
+            fileWriter.write("]")
+            fileWriter.close()
+          } else {
+            fileWriter.write(s"$json,\n")
+          }
+        case Xor.Left(e) => println(s"error: $e")
+      }
+    }
+    require(task != null)
+  }
+
+  def daysBetween(fromDate: LocalDate, toDate: LocalDate): Seq[LocalDate] =
+    fromDate.toEpochDay.until(toDate.toEpochDay).map(LocalDate.ofEpochDay)
+}
