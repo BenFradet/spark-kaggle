2c7a871ac13d54cecf05954805c306d4af87ff7a
BenFradet <benjamin.fradet@gmail.com>
2016-06-05 16:55:18 +0200

    modified the submit script to take into account the weather dataset

--- sfCrime/bin/submit.sh
+++ sfCrime/bin/submit.sh
@@ -16,6 +16,7 @@ TRAIN_PATH="${RESOURCES_PATH}${TRAIN_FILE}"
 TEST_FILE="test.csv"
 TEST_PATH="${RESOURCES_PATH}${TEST_FILE}"
 SUNSET_PATH="${RESOURCES_PATH}sunsetrise.json"
+WEATHER_PATH="${RESOURCES_PATH}weather.json"
 
 unzip ${TRAIN_PATH} -d ${RESOURCES_PATH}
 unzip ${TEST_PATH} -d ${RESOURCES_PATH}
@@ -26,7 +27,7 @@ spark-submit \
   --driver-memory 2G \
   --executor-memory 4G \
   target/scala-2.11/sfCrime-assembly-1.0.jar \
-  ${TRAIN_PATH} ${TEST_PATH} ${SUNSET_PATH} ${OUTPUT}
+  ${TRAIN_PATH} ${TEST_PATH} ${SUNSET_PATH} ${WEATHER_PATH} ${OUTPUT}
 
 mv ${OUTPUT}/part-00000 ${TMP_FILE}
 rm -rf ${OUTPUT}
