e799aa2a1238f57f29c283abf3d0bfc3fd8a2e09
BenFradet <benjamin.fradet@gmail.com>
2016-06-06 22:58:17 +0200

    submit script now taking into account the neighborhoods dataset

--- sfCrime/bin/submit.sh
+++ sfCrime/bin/submit.sh
@@ -17,6 +17,7 @@ TEST_FILE="test.csv"
 TEST_PATH="${RESOURCES_PATH}${TEST_FILE}"
 SUNSET_PATH="${RESOURCES_PATH}sunsetrise.json"
 WEATHER_PATH="${RESOURCES_PATH}weather.json"
+NBHD_PATH="${RESOURCES_PATH}neighborhoods.json"
 
 unzip ${TRAIN_PATH} -d ${RESOURCES_PATH}
 unzip ${TEST_PATH} -d ${RESOURCES_PATH}
@@ -27,7 +28,7 @@ spark-submit \
   --driver-memory 2G \
   --executor-memory 4G \
   target/scala-2.11/sfCrime-assembly-1.0.jar \
-  ${TRAIN_PATH} ${TEST_PATH} ${SUNSET_PATH} ${WEATHER_PATH} ${OUTPUT}
+  ${TRAIN_PATH} ${TEST_PATH} ${SUNSET_PATH} ${WEATHER_PATH} ${NBHD_PATH} ${OUTPUT}
 
 mv ${OUTPUT}/part-00000 ${TMP_FILE}
 rm -rf ${OUTPUT}
